95cb700ab8f2781debf31b0dd34a308a22197085
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2023-04-05 16:59:24 +0700

    finish

--- UCP_IBD_1/Program.cs
+++ UCP_IBD_1/Program.cs
@@ -9,40 +9,7 @@ namespace UCP_IBD_1
 {
     internal class Program
     {
-        public void insert(string id_calon_mhs, string nama, string alamat, string no_telp, string ttl, string email, SqlConnection con)
-        {
-            string str = "";
-            str = "insert into HRD.calon_mhs (idCalon_Mhs, Nama, Alamat, NoTelp, Ttl, Email)"
-                + "values(@idCalon, @nma, @alamat, @Phn, @ttl, @email)";
-
-            SqlCommand cmd = new SqlCommand(str, con);
-            cmd.CommandType = System.Data.CommandType.Text;
-
-            cmd.Parameters.Add(new SqlParameter("idCalon", id_calon_mhs));
-            cmd.Parameters.Add(new SqlParameter("nma", nama));
-            cmd.Parameters.Add(new SqlParameter("alamat", alamat));
-            cmd.Parameters.Add(new SqlParameter("Phn", no_telp));
-            cmd.Parameters.Add(new SqlParameter("ttl", ttl));
-            cmd.Parameters.Add(new SqlParameter("email", email));
-
-            cmd.ExecuteNonQuery();
-            Console.WriteLine("Data Berhasil Ditambahkan");
-        }
-
-        public void baca (SqlConnection con)
-        {
-            SqlCommand cmd = new SqlCommand("Select + from HRD.calon_mhs", con);
-            SqlDataReader r = cmd.ExecuteReader();
-            while (r.Read())
-            {
-                for (int i = 0; i < r.FieldCount; i++)
-                {
-                    Console.WriteLine(r.GetValue(i));
-                }
-                Console.WriteLine();
-            }
-            r.Close();
-        }
+        
         
         static void Main(string[] args)
         {
@@ -139,11 +106,53 @@ namespace UCP_IBD_1
                             }
                             default:
                             {
-                                Console.WriteLine("Invalid Opri")
+                                Console.WriteLine("Invalid Option");
                             }
+                            break;
                     }
                 }
+                catch
+                {
+                    Console.Clear();
+                    Console.ForegroundColor= ConsoleColor.Green;
+                    Console.WriteLine("Tidak dapat mengakses database menggunakan user tersebut\n");
+                    Console.ResetColor();
+                }
             }
+        }
+        public void insert(string id_calon_mhs, string nama, string alamat, string no_telp, string ttl, string email, SqlConnection con)
+        {
+            string str = "";
+            str = "insert into HRD.calon_mhs (idCalon_Mhs, Nama, Alamat, NoTelp, Ttl, Email)"
+                + "values(@idCalon, @nma, @alamat, @Phn, @ttl, @email)";
+
+            SqlCommand cmd = new SqlCommand(str, con);
+            cmd.CommandType = System.Data.CommandType.Text;
+
+            cmd.Parameters.Add(new SqlParameter("idCalon", id_calon_mhs));
+            cmd.Parameters.Add(new SqlParameter("nma", nama));
+            cmd.Parameters.Add(new SqlParameter("alamat", alamat));
+            cmd.Parameters.Add(new SqlParameter("Phn", no_telp));
+            cmd.Parameters.Add(new SqlParameter("ttl", ttl));
+            cmd.Parameters.Add(new SqlParameter("email", email));
+
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Data Berhasil Ditambahkan");
+        }
+
+        public void baca(SqlConnection con)
+        {
+            SqlCommand cmd = new SqlCommand("Select + from HRD.calon_mhs", con);
+            SqlDataReader r = cmd.ExecuteReader();
+            while (r.Read())
+            {
+                for (int i = 0; i < r.FieldCount; i++)
+                {
+                    Console.WriteLine(r.GetValue(i));
+                }
+                Console.WriteLine();
+            }
+            r.Close();
         }
     }
 }
